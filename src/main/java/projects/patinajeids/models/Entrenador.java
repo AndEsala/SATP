625016fba7eb4e99289ee01d23f34d19858a207a
AndEsala <andresesala25@outlook.com>
2024-05-19 01:26:49 -0500

    Entrenador Entity Created

---
+++ src/main/java/projects/patinajeids/models/Entrenador.java
@@ -0,0 +1,80 @@
+package projects.patinajeids.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "treinadores")
+public class Entrenador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "cpf")
+    private String cedula;
+
+    @Column(name = "nome")
+    private String nombre;
+
+    @Column(name = "p_sobrenome")
+    private String primerApellido;
+
+    @Column(name = "s_sobrenome")
+    private String segundoApellido;
+
+    @Column(name = "telefone")
+    private String telefono;
+
+    /* Getters & Setters */
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
