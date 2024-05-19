fa438ea76bcc3da87c07181bc1af7243f7831441
AndEsala <andresesala25@outlook.com>
2024-05-19 01:26:36 -0500

    Deportista Entity created

---
+++ src/main/java/projects/patinajeids/models/Deportista.java
@@ -0,0 +1,129 @@
+package projects.patinajeids.models;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "deportistas")
+public class Deportista {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "doc_identidad")
+    private String docIdentidad;
+
+    @Column(name = "nome")
+    private String nombre;
+
+    @Column(name = "p_sobrenome")
+    private String primerApellido;
+
+    @Column(name = "s_sobrenome")
+    private String segundoApellido;
+
+    @Column(name = "data_nascimento")
+    private Date fechaNacimiento;
+
+    @Column(name = "idade")
+    private Integer edad;
+
+    private String sexo;
+
+    @OneToOne
+    @JoinColumn(name = "id_club")
+    private Club club;
+
+    @OneToOne
+    @JoinColumn(name = "id_categoria")
+    private Categoria categoria;
+
+    /* Getters & Setters */
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDocIdentidad() {
+        return docIdentidad;
+    }
+
+    public void setDocIdentidad(String docIdentidad) {
+        this.docIdentidad = docIdentidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public Club getClub() {
+        return club;
+    }
+
+    public void setClub(Club club) {
+        this.club = club;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
