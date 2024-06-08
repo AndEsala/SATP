5793e35506a8eb71777d266748f8b6d863af06a9
AndEsala <andresesala25@outlook.com>
2024-06-08 15:37:43 -0500

    Clase Entidad `Bateria`

---
+++ src/main/java/projects/patinajeids/models/Bateria.java
@@ -0,0 +1,46 @@
+package projects.patinajeids.models;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "baterias")
+public class Bateria {
+    @Id
+    @Column(name = "id")
+    private Integer idBateria;
+
+    @ManyToMany
+    @JoinTable(
+        name = "baterias_deportistas",
+        joinColumns = @JoinColumn(name = "id_bateria"),
+        inverseJoinColumns = @JoinColumn(name = "id_deportista")
+    )
+    private List<Deportista> deportistas;
+
+    /* Getters & Setters */
+    public Integer getIdBateria() {
+        return idBateria;
+    }
+
+    public void setIdBateria(Integer idBateria) {
+        this.idBateria = idBateria;
+    }
+
+    public List<Deportista> getDeportistas() {
+        return deportistas;
+    }
+
+    public void setDeportistas(List<Deportista> deportistas) {
+        this.deportistas = deportistas;
+    }
+
+    
+}
