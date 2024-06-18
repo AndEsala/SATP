9b24872413c3ac3d996c8d11ffe0f26d502771d1
AndEsala <andresesala25@outlook.com>
2024-06-17 21:22:38 -0500

    Adición de Getters & Setters para InscripciónId

--- src/main/java/projects/patinajeids/models/InscripcionId.java
+++ src/main/java/projects/patinajeids/models/InscripcionId.java
@@ -17,4 +17,29 @@ public class InscripcionId {
     @ManyToOne
     @JoinColumn(name = "id_deportista")
     private Deportista deportista;
+
+    /* Getters & Setters */
+    public Torneo getTorneo() {
+        return torneo;
+    }
+
+    public void setTorneo(Torneo torneo) {
+        this.torneo = torneo;
+    }
+
+    public Club getClub() {
+        return club;
+    }
+
+    public void setClub(Club club) {
+        this.club = club;
+    }
+
+    public Deportista getDeportista() {
+        return deportista;
+    }
+
+    public void setDeportista(Deportista deportista) {
+        this.deportista = deportista;
+    }
 }
