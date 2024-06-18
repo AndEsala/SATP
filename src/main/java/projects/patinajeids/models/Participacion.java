dfe8cf3cfdd73b92f30ef3782474269956af6d98
AndEsala <andresesala25@outlook.com>
2024-06-17 21:22:50 -0500

    Adición de validaciones para Participación

--- src/main/java/projects/patinajeids/models/Participacion.java
+++ src/main/java/projects/patinajeids/models/Participacion.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "participaiones")
@@ -14,12 +15,17 @@ public class Participacion {
     @EmbeddedId
     private ParticipacionId id;
 
+    @NotNull(message = "La posición no puede estar vacía!")
     private Integer posicion;
+
+    @NotNull(message = "Los puntos no pueden estar vacíos!")
     private Integer puntos;
 
+    @NotNull(message = "El tiempo no puede estar vacío!")
     @Temporal(TemporalType.TIME)
     private Date tiempo;
 
+    @NotNull(message = "La fecha no puede estar vacía!")
     @Temporal(TemporalType.TIMESTAMP)
     private Date fecha;
 
