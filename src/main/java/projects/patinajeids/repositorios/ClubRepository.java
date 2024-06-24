10524bb13072a5ffd84506a53da3627e36d1caab
AndEsala <andresesala25@outlook.com>
2024-06-24 11:29:59 -0500

    Adición de Métodos para Obtener Clubes Inscritos y Pagos Totales

--- src/main/java/projects/patinajeids/repositorios/ClubRepository.java
+++ src/main/java/projects/patinajeids/repositorios/ClubRepository.java
@@ -1,5 +1,6 @@
 package projects.patinajeids.repositorios;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,7 +13,12 @@ public interface ClubRepository extends JpaRepository<Club, Integer> {
     @Query(value = "SELECT * FROM clubes c WHERE id = clubCampeon(:idTorneo)", nativeQuery = true)
     Optional<Club> findByIdChampion(Integer idTorneo);
     
-    @Query(value = "SELECT TotalPuntosClub(:idTorneo, :idClub)", nativeQuery = true)
-    Integer GetPuntosClub(Integer idTorneo, Integer idClub);
+    @Query(value = "SELECT totalPuntosClub(:idTorneo, :idClub)", nativeQuery = true)
+    Integer getPuntosClub(Integer idTorneo, Integer idClub);
 
+    @Query(value = "SELECT * FROM clubes WHERE id IN (SELECT DISTINCT c.id FROM inscripciones i JOIN deportistas d ON i.id_deportista = d.id JOIN clubes c ON i.id_club = c.id WHERE i.id_torneo = :idTorneo", nativeQuery = true)
+    List<Club> getClubesInscritos(Integer idTorneo);
+
+    @Query(value = "SELECT SUM(pago) FROM inscripciones WHERE id_torneo = :idTorneo AND id_club = :idClub", nativeQuery = true)
+	Float getTotalPagos(Integer idTorneo, Integer idClub);
 }
