22548a9eabcb46f243dafc19a10d400e23ac7f54
AndEsala <andresesala25@outlook.com>
2024-07-04 11:21:35 -0500

    Correción de Consulta

--- src/main/java/projects/patinajeids/repositorios/ClubRepository.java
+++ src/main/java/projects/patinajeids/repositorios/ClubRepository.java
@@ -15,7 +15,7 @@ public interface ClubRepository extends JpaRepository<Club, Integer> {
     @Query(value = "SELECT totalPuntosClub(:idTorneo, :idClub)", nativeQuery = true)
     Integer getPuntosClub(Integer idTorneo, Integer idClub);
 
-    @Query(value = "SELECT * FROM clubes WHERE id IN (SELECT DISTINCT c.id FROM inscripciones i JOIN deportistas d ON i.id_deportista = d.id JOIN clubes c ON i.id_club = c.id WHERE i.id_torneo = :idTorneo", nativeQuery = true)
+    @Query(value = "SELECT * FROM clubes WHERE id IN (SELECT DISTINCT c.id FROM inscripciones i JOIN deportistas d ON i.id_deportista = d.id JOIN clubes c ON i.id_club = c.id WHERE i.id_torneo = :idTorneo)", nativeQuery = true)
     List<Club> getClubesInscritos(Integer idTorneo);
 
     @Query(value = "SELECT SUM(pago) FROM inscripciones WHERE id_torneo = :idTorneo AND id_club = :idClub", nativeQuery = true)
